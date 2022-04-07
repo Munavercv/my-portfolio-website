09ef625a139cf2747fc8d80e40fcf39331c6273b
Munavercv <munavermunu80@gmail.com>
2022-04-07 11:49:14 +0530

    INITIAL COMMIT                                                                  created header

---
+++ script.js
@@ -0,0 +1,25 @@
+// menu show
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// active and remove menu
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
